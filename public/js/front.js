10665710a6ee1a6e4569669ada3c0742e329d714
casa92 <casartelli92@gmail.com>
2022-03-25 12:27:31 +0100

    prova

--- public/js/front.js
+++ public/js/front.js
@@ -19636,7 +19636,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Giuseppe\Classe#48\repository\BoolBnB\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\user\Boolean\BoolBnB\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
