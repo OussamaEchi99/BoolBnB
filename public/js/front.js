04d57415a5c8ccbd8d8f6799eddafa2afe5866c1
Martina-LP <madlp8@gmail.com>
2022-03-27 16:32:48 +0200

    pull

--- public/js/front.js
+++ public/js/front.js
@@ -19934,11 +19934,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-module.exports = __webpack_require__(/*! C:\Users\Alber\Desktop\Boolean\Progetto.finale\BoolBnB\resources\js\front.js */"./resources/js/front.js");
-=======
-module.exports = __webpack_require__(/*! C:\Users\Giuseppe\Classe#48\repository\BoolBnB\resources\js\front.js */"./resources/js/front.js");
->>>>>>> 096c9fb3f4a6296b6b9eec9f69802c4bad6a2405
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\boolbnb\boolbnb\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
