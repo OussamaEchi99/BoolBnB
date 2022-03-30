e72788955b4def9618c9d8cc702a4c9dcd7ee745
Martina-LP <madlp8@gmail.com>
2022-03-30 20:05:54 +0200

    Edit

--- resources/views/host/locations/index.blade.php
+++ resources/views/host/locations/index.blade.php
@@ -2,25 +2,25 @@
 
 @section('content')
 <div class="container">
-    <h1 class="mb-3">Le mie locations</h1>
+    <h1 class="mb-3">Le mie location</h1>
     <div class="row row-cols-3">
         @foreach ($locations as $location)
             @if ($location->user_id == Auth::id())
                 {{-- Single location --}}
                 <div class="col">
                     <div class="card mt-2">
-                        
+
                         @if (str_contains($location->photo, 'location_photos'))
                             <img src="{{ asset('storage/' . $location->photo) }}" class="card-img-top" alt="{{ $location->name }}">
                         @else
                             <img src="{{ $location->photo }}" class="card-img-top" alt="{{ $location->name }}">
                         @endif
-                        
+
 
                         <div class="card-body">
                         <h5 class="card-title">{{ $location->name }}</h5>
                         <p class="card-text">{{ Str::substr($location->description, 0, 70) }}...</p>
-                        <a href="{{ route('host.locations.show', ['location' => $location->id]) }}" class="btn btn-primary">Vai alla casa</a>
+                        <a href="{{ route('host.locations.show', ['location' => $location->id]) }}" class="btn btn-primary">Vai alla location</a>
                         </div>
                     </div>
                 </div>
