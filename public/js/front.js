dc3f91a3d14f34df18fd51ef297ab855d6776cde
Bisio98 <alberto.bisetti@hotmail.it>
2022-03-31 19:15:35 +0200

    edit sistemato

--- public/js/front.js
+++ public/js/front.js
@@ -20646,11 +20646,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 module.exports = __webpack_require__(/*! C:\Users\Alber\Desktop\Boolean\Progetto.finale\BoolBnB\resources\js\front.js */"./resources/js/front.js");
-=======
-module.exports = __webpack_require__(/*! C:\Users\Giuseppe\Classe#48\repository\BoolBnB\resources\js\front.js */"./resources/js/front.js");
->>>>>>> 17b3d98e78969d6fc45ab96445b9768c321727c5
 
 
 /***/ })
