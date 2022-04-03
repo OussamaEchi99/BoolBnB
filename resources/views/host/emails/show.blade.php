7ad1bdb96299b52ded47e40cdd70a935038d6ca3
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-04-03 17:53:49 +0200

    Sistemato lista mail

--- resources/views/host/emails/show.blade.php
+++ resources/views/host/emails/show.blade.php
@@ -1,20 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <strong>Email ricevute per: {{$location[0]->name}}</strong>
-    @if (!$emails->isEmpty())
-        @foreach ($emails as $email)
-            <div class="card my-4">
-                <div class="card-header"><strong>Oggetto:</strong> {{$email->object}}</div>
-                <div class="card-body">
-                    <div class="card-title"><strong>Da:</strong> {{$email->name}} ({{$email->email}})</div>
-                    <p class="card-text"><strong>Messaggio:</strong> {{$email->message}}</p>
+    
+    <div class="container">
+        {{-- <strong>Email ricevute per: {{$location[0]->name}}</strong> --}}
+        <h4>Email ricevute per:  <strong>{{strtoupper($location[0]->name)}}</strong></h4>
+        @if (!$emails->isEmpty())
+            @foreach ($emails as $email)
+                <div class="card my-4">
+                    <div class="card-header"><strong>Oggetto:</strong> {{$email->object}}</div>
+                    <div class="card-body">
+                        <div class="card-title"><strong>Da:</strong> {{$email->name}} ({{$email->email}})</div>
+                        <p class="card-text"><strong>Messaggio:</strong> {{$email->message}}</p>
+                    </div>
                 </div>
-            </div>
-        @endforeach
-    @else
-        <div style="font-size: 30px">Non ci sono email per questa location</div>
-    @endif
-
+            @endforeach
+        @else
+            <div style="font-size: 30px">Non ci sono email per questa location</div>
+        @endif
+    </div>
 
 @endsection
